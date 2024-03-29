65265338f9cec20478575467ee9637927339fe42
MikailSari1 <m.sari1@student.avans.nl>
2024-03-29 22:08:05 +0100

    alle queries opgeslagen

--- DB2/SQLQuerys.sql
+++ DB2/SQLQuerys.sql
@@ -45,3 +45,75 @@ GROUP BY
     title
 ORDER BY
     totalWatchTime_minutes DESC;
+
+    -- student aanmaken
+
+INSERT INTO student (emailAddress, name, birthdate, gender, address, city, country) VALUES (?, ?, ?, ?, ?, ?, ?)
+
+-- cursus aanmaken
+
+INSERT INTO course (courseName, subject, introductionText, level) VALUES (?, ?, ?, ?)
+
+-- module aanmaken
+
+INSERT INTO module (title, version, description, contactPersonName, contactPersonEmail, courseID) VALUES (?, ?, ?, ?, ?, ?)
+
+-- webcast aanmaken
+
+INSERT INTO webcast (title, description, speakerName, speakerOrganization, duration, publicationDate, url) VALUES (?, ?, ?, ?, ?, ?, ?)
+
+-- inschrijving aanmaken
+
+INSERT INTO enrollment (studentEmailAddress, courseID, enrollmentDate) VALUES (?, ?, ?)
+
+-- progress aanmaken
+
+INSERT INTO progress (enrollmentID, contentItemID, percentageWatched) VALUES (?, ?, ?)
+
+-- alle studenten ophalen
+
+SELECT * FROM student
+
+-- alle cursussen ophalen
+
+SELECT * FROM course
+
+-- alle modules ophalen
+
+SELECT * FROM module
+
+-- alle webcasts ophalen
+
+SELECT * FROM webcast
+
+-- alle inschrijvingen ophalen
+
+SELECT * FROM enrollment
+
+-- alle progress ophalen
+
+SELECT * FROM progress
+
+-- studenten updaten
+
+UPDATE student SET name = ?, birthdate = ?, gender = ?, address = ?, city = ?, country = ? WHERE emailAddress = ?
+
+-- cursussen updaten
+
+UPDATE course SET courseName = ?, subject = ?, introductionText = ?, level = ? WHERE courseID = ?
+
+-- modules updaten
+
+UPDATE module SET title = ?, version = ?, description = ?, contactPersonName = ?, contactPersonEmail = ?, courseID = ? WHERE moduleID = ?
+
+-- webcasts updaten
+
+UPDATE webcast SET title = ?, description = ?, speakerName = ?, speakerOrganization = ?, duration = ?, publicationDate = ?, url = ? WHERE webcastID = ?
+
+-- inschrijvingen updaten
+
+UPDATE enrollment SET studentEmailAddress = ?, courseID = ?, enrollmentDate = ? WHERE enrollmentID = ?
+
+-- progress updaten
+
+UPDATE progress SET enrollmentID = ?, contentItemID = ?, percentageWatched = ? WHERE progressID = ?
